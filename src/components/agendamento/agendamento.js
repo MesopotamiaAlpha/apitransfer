91b03cd74ace06b7fa47546b54b5acd626e4b40e
Jose Antonio <leao-gomes@hotmail.com>
2023-05-13 23:14:16 -0300

    Adição de campos para cadastro

--- src/components/agendamento/agendamento.js
+++ src/components/agendamento/agendamento.js
@@ -12,6 +12,12 @@ import SendIcon from '@material-ui/icons/Send';
 function Agendamento_cli() {
 
     const [userDestinatario, setDestinatario] = useState("");
+    const [userEndPartida, setEndPartida] = useState("");
+    const [userPontoRefPart, setPontoRefPart] = useState("");
+    const [useNumEndPartida, setNumEndPartida] = useState("");
+    const [useEndDestino, setEndDestino] = useState("");
+    const [useNumEndDestino, setNumEndDestino] = useState("");
+    const [userPontoRefDest, setPontoRefDest] = useState("");
     let [userTipo, setTipo] = useState("");
     const [userCte, setCte] = useState("");
     const [userData, setData] = useState("");
@@ -33,8 +39,14 @@ function Agendamento_cli() {
         <div className="quadro1">
             <Paper elevation={12} style={{ padding: 1, backgroundColor: "#e55e16", border: "5px solid #002021" }}  >
                 <h1 className="titulo2">Fazer pedido de Transfer</h1>
-                <input className="inpNomeCompleto" type="text" placeholder="Nome completo" onChange={(e) => {setDestinatario(e.target.value) }} />
-                <input className="inpTelefone" type="number" placeholder="Telefone" onChange={(e) => { setCte(e.target.value) }} />
+                <input className="inpNomeCompleto" type="text" placeholder="Nome completo*" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpEndPartida" type="text" placeholder="Endereço de partida" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpNumEndPartida" type="number" placeholder="Numero do Endereço de partida" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpPontoRefPart" type="text" placeholder="Ponto de referencia Partida" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpEndDestino" type="text" placeholder="Endereço de destino" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpNumEndDEstino" type="number" placeholder="Numero do Endereço de destino" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpPontoRefDestino" type="text" placeholder="Ponto de referencia Destino" onChange={(e) => {setDestinatario(e.target.value) }} />
+                <input className="inpTelefone" type="number" placeholder="Telefone com DDD*" onChange={(e) => { setCte(e.target.value) }} />
                 <input className="inpData" type="date" onChange={(e) => { setData(e.target.value) }} />
 
                 <div className="boxTipo">
@@ -42,13 +54,20 @@ function Agendamento_cli() {
                         userTipo = "Dinheiro";
                         console.log(userTipo);
                     }} />
+                    <p className="textoTipo" >Dinheiro *</p>
 
-                    <p className="textoTipo" >Dinheiro</p>
                     <Checkbox className="boxTipo" style={{ color: "white"}} onClick={() => {
                         userTipo = "Cartão";
                         console.log(userTipo);
                     }} />
-                    <p className="textoTipo">Cartão</p>
+                    <p className="textoTipo">Cartão de Crédito *</p>
+
+
+                    <Checkbox className="boxTipo" style={{ color: "white"}} onClick={() => {
+                        userTipo = "Cartão";
+                        console.log(userTipo);
+                    }} />
+                    <p className="textoTipo">Cartão de Débito</p>
                 </div>
 
 
