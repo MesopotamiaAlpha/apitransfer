03c2febd4314e87a512e1386ac51fb159f6b023a
Jose Antonio <leao-gomes@hotmail.com>
2023-05-13 23:14:32 -0300

    Retirando paper e adicionando List, mais ajustes

--- src/components/cliente/resultado.js
+++ src/components/cliente/resultado.js
@@ -2,51 +2,86 @@ import React, { useState, useEffect } from "react";
 import "./resultado";
 import "./resultado.css";
 import Axios from "axios";
-import { ListItemButton, Button, Paper } from '@mui/material';
+import { Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+const url = "http://192.168.0.115:3002/api";
 
-const url = "http://192.168.0.114:3002/api";
-
-
-// esta função traz todos os registros dentro do banco de dados e usa a função map para criar um array de objetos
 function Resultado_cli() {
     const [postList, setPostList] = useState([]);
 
-    useEffect((data) => {
-        Axios.get(url + "/get").then((data) => {
-            setPostList(data.data);
-            console.log(data.data);
+    useEffect(() => {
+        Axios.get(url + "/get").then((response) => {
+            setPostList(response.data);
         });
     }, [postList]);
 
     function deletPost(id) {
         Axios.delete(url + `/delete/${id}`).then((response) => {
-            window.alert("you deleted a post");
+          window.alert("you deleted a post");
         });
+      }
+
+    function handleDone(id) {
+        const newPostList = postList.map(post => {
+            if (post.id_pri === id) {
+                post.done = !post.done;
+            }
+            return post;
+        });
+        setPostList(newPostList);
     }
 
-    function deletRegistro() {
-        window.alert("Registro deletado com sucesso!");
-    }
-
-    // dentro do return ele verifica o array e cria um botão para deletar para cada registro dentro do banco de dados, os botoes possuem um id de cada registro para apagar cada um    
     return (
-
         <Paper className="paper" elevation={12} style={{ padding: 1, backgroundColor: "#eaeef3" }} sx={{ mx: 5, textAlign: 'center' }} >
             <div className="resultado">
-
                 <h1 className="titulo">Transfers solicitados</h1>
-                <ul className="listaUl">
-
-                    {postList.map(nome_prinex =>
-                        <ListItemButton style={{ backgroundColor: "#eaeef3" }} component="a" className="textoLista" key={nome_prinex.id_pri}>
-
-                            {"Nome do passageiro: " + nome_prinex.destinatario + "   / Tipo Pagamento: " + nome_prinex.tipo + "   / Numero Telefone: " + nome_prinex.cte + "   / Data da viagem: " + nome_prinex.cad_data + "Id Banco:" + nome_prinex.id_pri }
-                            
-                            <Button className="btnDel" onClick={(() => deletPost(nome_prinex.id_pri))}> <DeleteIcon edge="end"  onClick={deletRegistro}/> </Button>
-                        </ListItemButton>)}
-                </ul>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Concluído</TableCell>
+                                <TableCell>Nome do passageiro</TableCell>
+                                <TableCell>Tipo de pagamento</TableCell>
+                                <TableCell>Número do telefone</TableCell>
+                                <TableCell>Data da viagem</TableCell>
+                                <TableCell>Id do Banco</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {postList.length ? (
+                                postList.map((nome_prinex) => (
+                                    <TableRow
+                                        key={nome_prinex.id_pri}
+                                        style={{ backgroundColor: nome_prinex.done ? "green" : "#eaeef3" }}
+                                    >
+                                        <TableCell>
+                                            <Checkbox
+                                                checked={nome_prinex.done}
+                                                onChange={() => handleDone(nome_prinex.id_pri)}
+                                            />
+                                        </TableCell>
+                                        <TableCell>{nome_prinex.destinatario}</TableCell>
+                                        <TableCell>{nome_prinex.tipo}</TableCell>
+                                        <TableCell>{nome_prinex.cte}</TableCell>
+                                        <TableCell>{nome_prinex.cad_data}</TableCell>
+                                        <TableCell>{nome_prinex.id_pri}</TableCell>
+                                        <TableCell>
+                                        <Button className="btnDel" onClick={() => deletPost(nome_prinex.id_pri)}>
+                        <DeleteIcon/>
+                      </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell colSpan={7} align="center">Carregando...</TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </Paper>
     );
